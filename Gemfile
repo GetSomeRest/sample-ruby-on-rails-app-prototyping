f09665457b89f7dcd65b40385208d3166ec365d8
prathamalag1994 <prathamalag@berkeley.edu>
2015-06-22 13:58:33 -0700

    Testing out curl responses

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
 gem 'rails_12factor'
+gem 'curb'
+gem 'curb-fu'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+	response = CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => current_user.key, :client_secret => current_user.secret, :grant_type => 'client_credentials' })
+  	puts "startstartstartstartstartstartstart"
+  	puts response.body
+  	puts "endendendendendendendendendendend"
+  	@t = response.body
   	@user = current_user
   end
 end
