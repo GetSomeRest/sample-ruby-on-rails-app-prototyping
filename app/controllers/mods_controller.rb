66de3aabb78b94dced14a3cf07df5c8b053dda88
prathamalag1994 <prathamalag@berkeley.edu>
2015-07-06 15:39:00 -0700

    Fixed all user views to work perfectly

--- app/controllers/mods_controller.rb
+++ app/controllers/mods_controller.rb
@@ -5,7 +5,17 @@ class ModsController < ApplicationController
   def create
   	@mod = Mod.new(mod_params)
  
-  	@mod.save
+  	if @mod.save
+      if @mod.latest
+        if Mod.where(:uid => current_user.id).where(:latest => true).count > 1
+          @change = Mod.where(:uid => current_user.id).where(:latest => true).first
+          @change.latest = false
+          @change.save
+        end
+        @mod.latest = true
+      end
+    end
+    @mod.save
   	redirect_to '/view'
   end
   private
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   	@user = current_user
-    gon.urn = "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE1LTA3LTAxLTA4LTM1LTExLXl0a3RxY2huZG93M3NvejM5N2p3dnhzZWNvZjEvR2F0ZUhvdXNlLm53ZA=="
+    if Mod.all.count == 0
+      @status = "none"
+    elsif Mod.where(:uid => current_user.id).where(:latest => true).count != 0
+      gon.urn = Mod.where(:uid => current_user.id).where(:latest => true).first.urn
+    else
+      @status = "noviewable"
+    end
     token = JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => current_user.key, :client_secret => current_user.secret, :grant_type => 'client_credentials' }).body)
     gon.token = token["access_token"]
   end
