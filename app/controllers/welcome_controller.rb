780b721a1c8fa765edb284cb699038ed79ac3cb9
prathamalag1994 <prathamalag@berkeley.edu>
2015-07-09 13:44:40 -0700

    Changed view logic and views for judging

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,21 +8,24 @@ class WelcomeController < ApplicationController
     current_user.save
     if current_user.judge
       @status = "judge"
+      @users = User.all
     else
       if request.original_url.include?("urn")
         @status = "works"
+        token = JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => current_user.key, :client_secret => current_user.secret, :grant_type => 'client_credentials' }).body)
+        gon.token = token["access_token"]
       elsif Mod.all.count == 0
         @status = "none"
       elsif Mod.where(:uid => current_user.id).where(:latest => true).count != 0
         @status = "works"
         gon.urn = Mod.where(:uid => current_user.id).where(:latest => true).first.urn
+        token = JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => current_user.key, :client_secret => current_user.secret, :grant_type => 'client_credentials' }).body)
+        gon.token = token["access_token"]
       else
         @status = "noviewable"
       end
-      token = JSON.parse(CurbFu.post({:host => 'developer.api.autodesk.com', :path => '/authentication/v1/authenticate', :protocol => "https"}, { :client_id => current_user.key, :client_secret => current_user.secret, :grant_type => 'client_credentials' }).body)
-      gon.token = token["access_token"]
+      @user = current_user
     end
-    @user = current_user
   end
 
   def upload
@@ -34,6 +37,9 @@ class WelcomeController < ApplicationController
   end
 
   def view
+    if current_user.judge
+      @status = "judge"
+    end
     @mods = Mod.where(uid: current_user.id)
   end
 end
